73d1e8c2c8717278c38cebd435fddbc65febdaff
DenisProcyon <92217845+DenisProcyon@users.noreply.github.com>
2023-03-21 16:02:21 +0200

    Update LongStack.java
    Old version

--- src/LongStack.java
+++ src/LongStack.java
@@ -51,14 +51,14 @@ public class LongStack {
          } else if (s.equals("/")) {
             result = topmost_second / topmost_first;
          } else {
-            throw new IllegalArgumentException("Invalid operator :" + s);
+            throw new IllegalArgumentException("Invalid operator");
          }
 
          data.removeLast();
          data.removeLast();
          data.addLast(result);
 
-      } else throw new NoSuchElementException("Not enough elements in stack to perform (" + s + ") operation (<2)");
+      } else throw new NoSuchElementException();
    }
   
    public long tos() {
@@ -86,35 +86,20 @@ public class LongStack {
    public static long interpret(String pol) {
       LongStack new_stack = new LongStack();
 
-      if (pol == null){
-         throw new IllegalArgumentException("Argument is null");
-      }
-      if (pol.trim().isEmpty()){
-         throw new IllegalArgumentException("Pol is empty");
-      }
-
       String[] elements = pol.trim().split("\\s+");
       for (int i = 0; i < elements.length; i++) {
          String element = elements[i];
          if (element.matches("-?\\d+")) {
-            if (new_stack.data.size() >= 2 && elements.length - i < 2) {
-               // checks if there are still enough elements to perform expression
-               //System.out.println("Not enough numbers: " + pol);
-               throw new IllegalArgumentException("Not enough numbers: " + pol);
-            }
             new_stack.push(Long.parseLong(element.trim()));
          } else if (element.matches("[+\\-*/]")) {
             new_stack.op(element);
          } else {
-            // Checks if regex is not digit nor arithmetical operation
-            //System.out.println("Invalid symbol  (" + element + ") in " + pol);
-            throw new RuntimeException("Invalid symbol  (" + element + ") in " + pol);
+            throw new RuntimeException();
          }
       }
       if (new_stack.data.size() != 1) {
-         throw new RuntimeException("Too many numbers to perform expression: " + pol);
+         throw new RuntimeException();
       }
       return new_stack.tos();
    }
 }
-
